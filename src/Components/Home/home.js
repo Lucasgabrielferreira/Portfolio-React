259f752aaa7db1b62d19eac8d9c6386725856ed1
Lucas Gabriel <lg139826065@gmail.com>
2024-01-23 17:05:03 -0300

    Add Botão de Voltar ao topo

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./home.css";
 import Body from "../Body/body";
 import Header from "../Header/header";
 import Footer from "../Footer/footer";
 
 function Home() {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  useEffect(() => {
+    // Adiciona um listener para o evento de rolagem
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove o listener quando o componente é desmontado
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // Função para controlar a exibição do botão com base na posição de rolagem
+  const handleScroll = () => {
+    if (window.scrollY > 200) {
+      setShowScrollButton(true);
+    } else {
+      setShowScrollButton(false);
+    }
+  };
+
+  // Função para rolar para o topo da página
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // Adiciona uma animação suave
+    });
+  };
+
   return (
     <div className="home">
       <div>
@@ -19,6 +48,12 @@ function Home() {
         <Footer />
       </div>
 
+      {/* Botão "Voltar ao Topo" condicionalmente renderizado */}
+      {showScrollButton && (
+        <button className="scroll-top-button" onClick={scrollToTop}>
+          Voltar ao Topo
+        </button>
+      )}
     </div>
   );
 }
