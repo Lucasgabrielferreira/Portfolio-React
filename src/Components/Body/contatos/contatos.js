fa165896dfe1ead39b002510b5431bc40bb5280a
Lucas Gabriel <lg139826065@gmail.com>
2024-09-09 13:02:26 -0300

    Adicionando formulario

--- src/Components/Body/contatos/contatos.js
+++ src/Components/Body/contatos/contatos.js
@@ -10,10 +10,31 @@ function Contatos() {
         entrar em contato comigo!
       </p>
 
+      <section class="contact-form">
+  <h3 class="h3 form-title">Formulário de contato</h3>
+
+  <form action="https://formspree.io/f/xjkbzdny" method="POST" class="form" data-form>
+
+    <div class="input-group">
+      <input type="text" name="fullname" class="form-input" placeholder="Nome Completo" required data-form-input />
+      <input type="email" name="email" class="form-input" placeholder="Endereço de email" required data-form-input />
+    </div>
+
+    <textarea name="message" class="form-input" placeholder="Sua Mensagem" required data-form-input></textarea>
+
+    <button class="form-btn" type="submit">
+      <ion-icon name="paper-plane"></ion-icon>
+      <span>Enviar Mensagem</span>
+    </button>
+
+  </form>
+</section>
+
+
       <div className="informacoes-contato">
         <h3>Informações de Contato</h3>
-        <p>Email: seuemail@example.com</p>
-        <p>Telefone: (XX) XXXX-XXXX</p>
+        <p>Email: lg139826065@gmail.com</p>
+        <p>Telefone: (11) 94914-5048</p>
         {/* Adicione mais informações de contato, se necessário */}
       </div>
 
@@ -22,7 +43,7 @@ function Contatos() {
         <ul>
           <li>
             <a
-              href="https://www.linkedin.com/"
+              href="https://www.linkedin.com/in/lucas-gabriel-ferreira/"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -31,7 +52,7 @@ function Contatos() {
           </li>
           <li>
             <a
-              href="https://github.com/"
+              href="https://github.com/Lucasgabrielferreira/"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -41,6 +62,21 @@ function Contatos() {
           {/* Adicione mais redes sociais, se desejar */}
         </ul>
       </div>
+
+      <section className="mapbox" data-mapbox>
+  <figure>
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d468145.64808997244!2d-46.843295747149305!3d-23.554508954303742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce448183a461d1%3A0x9ba94b08ff335bae!2zU8OjbyBQYXVsbywgU1A!5e0!3m2!1spt-BR!2sbr!4v1725890732694!5m2!1spt-BR!2sbr"
+      width="600"
+      height="450"
+      style={{ border: 0 }} // Aqui o style está sendo passado como um objeto
+      allowFullScreen
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+      title="Mapa de São Paulo, SP"
+    ></iframe>
+  </figure>
+</section>
 
       {/* Adicione um formulário de contato, se necessário */}
     </div>
--- src/Components/Body/experiencias/experiencias.js
+++ src/Components/Body/experiencias/experiencias.js
@@ -7,10 +7,25 @@ function Experiencias() {
       <h2>Experiências Profissionais</h2>
       <div className="experiencia">
         <h3>Analista de Suporte Computacional</h3>
-        <p>RRSoft, São Paulo, Sp | Dezembro de 2022 - Atualmente.</p>
+        <p>RRSoft, São Paulo, Sp | Dezembro de 2022 - Maio de 2024.</p>
         <ul>
-          <li>Desenvolvimento e manutenção de interfaces de usuário responsivas.</li>
-          <li>Colaboração com equipes multidisciplinares para alcançar objetivos comuns.</li>
+          <li>Atendimento e Suporte Técnico aos Usuários: Proporcionava suporte técnico aos usuários dos softwares instalados pela empresa, solucionando problemas relatados de forma eficaz para assegurar o bom desempenho dos programas.
+          </li>
+          <li>Testagem de Programas: Realizava testes rigorosos em programas após o desenvolvimento e instalação, avaliando a qualidade e funcionamento para minimizar riscos de falhas e assegurar uma experiência de usuário estável.</li>
+          <li>Instalação, Configuração e Manutenção de Software e Hardware: Executava a instalação, configuração e manutenção de software e hardware, realizando testes funcionais e monitorando o desempenho dos equipamentos e sistemas para garantir a operacionalidade contínua e eficiente.
+          </li>
+          <li>Desenvolvimento em C# e .NET: Participava do desenvolvimento e manutenção de aplicações utilizando C# e .NET, contribuindo para a melhoria contínua dos sistemas internos. Consultas em MySQL: Realizava consultas e manipulações de dados em bancos de dados MySQL, suportando a extração e análise de informações essenciais para a tomada de decisões e resolução de problemas.</li>
+          {/* Adicione mais pontos relevantes da experiência profissional */}
+        </ul>
+      </div>
+
+      <div className="experiencia">
+        <h3>Auxiliar de estoque</h3>
+        <p>Vimap, São Paulo, Sp | Novembro de 2017 - Abril de 2022 .</p>
+        <ul>
+          <li>Gestão de Almoxarifado: Responsável por auxiliar no armazenamento e organização dos produtos no estoque, garantindo a correta disposição e acessibilidade dos itens.</li>
+          <li>Recebimento e Armazenagem de Mercadorias: Coordenava o recebimento das mercadorias, realizando a armazenagem de maneira eficiente para manter a integridade dos produtos e facilitar o controle do estoque.</li>
+          <li>Atendimento ao Cliente: Possuía experiência no atendimento a clientes internos e externos, fornecendo suporte eficaz e resolvendo dúvidas e problemas com agilidade.</li>
           {/* Adicione mais pontos relevantes da experiência profissional */}
         </ul>
       </div>
